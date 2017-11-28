15c61a20d01bc97302feb52e0577416f7311a1fe
GarciaPosada <GarciaPosada@192.168.1.134>
2017-11-28 23:57:49 +0100

    feat: se modifican las descriciones

--- spark-java-maven-example/src/main/java/com/examples/streaming/JavaSparkStreamingExample2.java
+++ spark-java-maven-example/src/main/java/com/examples/streaming/JavaSparkStreamingExample2.java
@@ -21,16 +21,13 @@ import org.apache.spark.streaming.kafka.KafkaUtils;
 import kafka.serializer.StringDecoder;
 
 /**
- * Counts words in UTF8 encoded, '\n' delimited text received from the network
- * every second.
+ * Consumes messages from one or more topics in Kafka and does wordcount.
+ * Usage: JavaDirectKafkaWordCount <brokers> <topics>
+ *   <brokers> is a list of one or more Kafka brokers
+ *   <topics> is a list of one or more kafka topics to consume from
  *
- * Usage: JavaNetworkWordCount <hostname> <port> <hostname> and <port> describe
- * the TCP server that Spark Streaming would connect to receive data.
- *
- * To run this on your local machine, you need to first run a Netcat server `$
- * nc -lk 9999` and then run the example `$ bin/run-example
- * org.apache.spark.examples.streaming.JavaNetworkWordCount localhost 9999`
  */
+
 public final class JavaSparkStreamingExample2 {
 
 	private static final Pattern SPACE = Pattern.compile(" ");
